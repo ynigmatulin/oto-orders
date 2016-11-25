51b93a1873d2639a4d5b28b5f62f06b014527be9
Ant Weiss <ant.weiss@gmail.com>
2016-11-25 16:09:51 +0200

    Explain component test script

--- tests/ct/run.sh
+++ tests/ct/run.sh
@@ -1,3 +1,7 @@
 #! /bin/bash -x
+# Component test for the orders service. 
+# Very dumb - create an order and verify there's an order for this product id.
+# Assumes a fresh environment
+
 curl -s -X POST -H "Content-Type:application/json" -d '{  "productId" : "13579",  "amount" : "3", "totalSum" : "12.5" }' ${CODEFRESH_ENV_URL}/orders
 curl -s ${CODEFRESH_ENV_URL}/orders | grep 13579
