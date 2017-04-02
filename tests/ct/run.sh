5986b0c33d4ae82fe49e0f010a7d6127cde55318
ant weiss <ant.weiss@gmail.com>
2017-04-02 06:22:35 +0000

    add http to test url

--- tests/ct/run.sh
+++ tests/ct/run.sh
@@ -3,5 +3,5 @@
 # Very dumb - create an order and verify there's an order for this product id.
 # Assumes a fresh environment
 
-curl -s -X POST -H "Content-Type:application/json" -d '{  "productId" : "13579",  "amount" : "3", "totalSum" : "12.5" }' ${APP_URL}/orders
-curl -s ${APP_URL}/orders | grep 13579
+curl -s -X POST -H "Content-Type:application/json" -d '{  "productId" : "13579",  "amount" : "3", "totalSum" : "12.5" }' http://${APP_URL}/orders
+curl -s http://${APP_URL}/orders | grep 13579
